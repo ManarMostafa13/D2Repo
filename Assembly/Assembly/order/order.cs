cccb413e5b088e8545f75d1574bfa93d545c781e
Mariam <mariam@gmail.com>
2023-09-18 13:26:04 +0300

    order module

---
+++ Assembly/Assembly/order/order.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public class Order
+{
+    public int Id { get; set; }
+    public User User { get; set; }
+    public float Price { get; set; }
+    public override string ToString()
+    {
+        return $"{Id}, {User?.Name}, {Price}";
+    }
+}
