c66742a2658767cde5976d383a551dbb5b2eacd5
Manar <eng.manar666@gmail.com>
2023-09-18 12:45:07 +0300

    user.dll

---
+++ Assembly/Assembly/User/userManeger.cs
@@ -0,0 +1,15 @@
+﻿public class UserManager
+{
+    private User[] users;
+    public UserManager()
+    {
+        users = new User[1]
+        {
+            new User(){Id=1, Name="Ahmed"}
+        };
+    }
+    public User[] Get()
+    {
+        return users;
+    }
+}
