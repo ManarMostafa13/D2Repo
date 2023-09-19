bf412f3308632f7e368ac513ab100b222110a60c
Manar <eng.manar666@gmail.com>
2023-09-19 13:32:03 +0300

    App.exe

--- Assembly/Assembly/userorder.cs
+++ Assembly/Assembly/userorder.cs
@@ -1,12 +1,12 @@
 ﻿using System.Reflection;
-[assembly: AssemblyVersion("2.0.0.0")]
+
 
 
 public class UserOrder
 {
     public static void Display()
     {
-        System.Console.WriteLine("V 2");
+        System.Console.WriteLine("1");
         UserManager mgr
              = new UserManager();
         System.Console.WriteLine(mgr.Get()[0].ToString());
